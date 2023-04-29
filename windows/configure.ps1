1b03da75430b9ce08a14cda5cd3e32e3b0fb256e
Claudio Fernandes <ddk.claudio@gmail.com>
2023-04-28 21:51:27 -0300

    feat: add script configure & python

---
+++ windows/configure.ps1
@@ -0,0 +1,7 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+choco install packages.config
+
+py -m ensurepip --upgrade
+py -m pip install --upgrade pip
+pip install pre-commit
